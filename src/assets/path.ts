d43f6df0e6ea3476fd1f97eb059eee091bf1cacf
vanduc0505205 <duclvph52846@gmail.com>
2025-07-24 21:10:31 +0700

    feat: update image

--- src/assets/path.ts
+++ src/assets/path.ts
@@ -1,2 +1,27 @@
-//images
-export const IMG_MAI = "@/assets/images/index/phim-mai.png";
+import nhiemVuBatKhaThi from '@/assets/images/index/nhiem_vu_bat_kha_thi.png';
+import superman from '@/assets/images/index/superman.png';
+import phimMai from '@/assets/images/index/phim-mai.png';
+import BKLR from '@/assets/images/index/bi_kip_luyen_rong.png';
+import phimXitrum from '@/assets/images/index/xi_trum.png';
+import deMen from '@/assets/images/index/de_men.png';
+import utlan from '@/assets/images/index/ut_lan.png';
+import trangQuynhNhi from '@/assets/images/index/trang_quynh.png';
+import dieuUocCuoiCuong from '@/assets/images/index/dieu_uoc_cuoi_cung.png';
+import maXuongMia from '@/assets/images/index/ma_xuong_mia.png';
+import motNuaHoanHao from '@/assets/images/index/mot_nua_hoan_hao.png';
+import elio from '@/assets/images/index/elio.png';
+
+export {
+    nhiemVuBatKhaThi,
+    superman,
+    phimMai,
+    BKLR,
+    phimXitrum,
+    deMen,
+    utlan,
+    trangQuynhNhi,
+    dieuUocCuoiCuong,
+    maXuongMia,
+    motNuaHoanHao,
+    elio,
+}
