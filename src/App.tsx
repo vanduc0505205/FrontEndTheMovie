118987bec79e5af992fc1d36b0bfcd8cec724f1e
ngocnt <ngocnthph48736@gmail.com>
2025-08-16 01:18:35 -0700

    feat: add profilepage

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ import RoomStaff from './staff/roomStaff/RoomAdmin'
 import OrderHistoryPage from './pages/OrderHistoryPage'
 import DiscountList from './admin/discountAdmin/discountList'
 import BookingAdmin from './admin/bookingAdmin/booking.admin'
+import ProfilePage from './pages/ProfilePage'
 
 
 
@@ -69,6 +70,7 @@ function App() {
         <Route path="/quy-dinh-va-chinh-sach" element={<PolicyPage />} />
         <Route path="/phim/:id/checkout" element={<Checkout />} />
         <Route path="/lichsudatve" element={<OrderHistoryPage />} />
+        <Route path="/thong-tin-ca-nhan" element={<ProfilePage />} />
         
       </Route>
 
